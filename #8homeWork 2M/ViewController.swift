942030281e1ccf41e617ccbed475623804a65c70
Nurik <N-TNT@MAIL.RU>
2023-02-07 19:01:15 +0600

    Initial Commit

---
+++ #8homeWork 2M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  #8homeWork 2M
+//
+//  Created by Нурлан on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
